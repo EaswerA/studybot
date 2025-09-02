2aa32e3dab2e2f40a360315e2701947fc015c0b1
EaswerA <easwer567@gmail.com>
2025-09-02 09:59:39 +0530

    changes

--- interface/streamlit_app.py
+++ interface/streamlit_app.py
@@ -1,6 +1,3 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import streamlit as st
 from engine.roadmap import RoadmapEngine
 from engine.tracker import ProgressTracker
